83a0933c10c0818a02788c979f3107f38b82943f
caryw1987 <weiwei2646@163.com>
2016-12-06 22:46:27 +0800

    update user dao and dto

--- src/main/java/com/zzty/demo/shopping/dao/DAO.java
+++ src/main/java/com/zzty/demo/shopping/dao/DAO.java
@@ -4,6 +4,11 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+/**
+ * 所有 DAO的父类， 封装了获取jdbc connection的操作， 这样自类就不需要重复的去写这些代码，
+ * 通过 getConnection() 就可以获取一个新的jdbc connection
+ *
+ */
 public class DAO {
 	
 	private String url = "jdbc:mysql://127.0.0.1:3306/online_shopping?user=root&password=root";
--- src/main/java/com/zzty/demo/shopping/dto/UserAddressDTO.java
+++ src/main/java/com/zzty/demo/shopping/dto/UserAddressDTO.java
@@ -5,28 +5,35 @@ public class UserAddressDTO {
 	private String address;
 	private boolean isDefault;
 	private UserDTO user;
-	
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getAddress() {
 		return address;
 	}
+
 	public void setAddress(String address) {
 		this.address = address;
 	}
+
 	public boolean isDefault() {
 		return isDefault;
 	}
+
 	public void setDefault(boolean isDefault) {
 		this.isDefault = isDefault;
 	}
+
 	public UserDTO getUser() {
 		return user;
 	}
+
 	public void setUser(UserDTO user) {
 		this.user = user;
 	}
